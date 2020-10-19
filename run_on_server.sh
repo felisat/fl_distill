7ee1bbf4de8237a054719afbfcf86a8bf6a5c170
Felix Sattler <felix.sattler@hhi.fraunhofer.de>
2020-10-19 17:03:11 +0200

    run experiment on cluster

--- run_on_server.sh
+++ run_on_server.sh
@@ -13,10 +13,5 @@ ssh -o "StrictHostKeyChecking no" fsattler@vca-gpu-211-01 << EOF
 	head -n 50 exec.sh
 	echo "Running $max experiments..."
 
-	for (( c=0; c<$max; c+=$step ))
-	do
-	   echo "$c"
-	   echo sbatch exec.sh "--start $c --end $(($c+$step))"
-	   #sbatch exec.sh "--start $c --end $(($c+$step))"
-	done
+	bash run_multiple.sh "$max $step"
 EOF
